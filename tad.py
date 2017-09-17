6d4ea46bf27a07cfa6f670279bf82b3248958bed
drivera1 <danir964@gmail.com>
2017-09-17 18:32:33 +0200

    Tad con errores
    Python pasa todo por valor, así que no podemos hacer cambios
    directamente al objeto que pasamos, pero a sus campos (key,left y right
    ) si, así que hay que modificarlas para poder hacer la operación,
    insertar recursiva ya está bien

--- tad.py
+++ tad.py
@@ -1,46 +1,236 @@
 class Node:
-	def createNode(self, key =None, left =None, right =None):
-		self.key = key
-		self.left = left
-		self.right = right
+	def __init__(abb):
+		abb.key = None
+		abb.left = None
+		abb.right = None
 
-	def emptyTRee(self):
-		self.key = None
-
-#--------------------------------------------------------------·#
-
+#------------------------Node Creation-----------------------------#
+def createNode():
+	return Node()
+	
+#------------------------Empty Tree-----------------------------#
+def emptyTree(abb):
+	abb.key = None
+	abb.left = None
+	abb.right = None
 
 #------------------------ Recursive Insert----------------------#
-	def insert_r(self,key):
-		if self.key == None:
-			createNode(self,key)
-		else :
-			if key < self.key:
-				insert_r(self.left,key)
-			else if key > self.key : 
-				insert_r(self.right,key)
 
 
-	def insert_i(self,key):
-		newNode = Node ()
-		createNode(newNode,key)
-		if self.key == None:
-			self.key = newNode.key
-			self.left = newNode.left
-			self.right = newNode.right			
-		else :
-			if newNode.key < self.key :
-				son = self
-				father = Node ()
-				while son.key != None && son.key != key:
-					father = son
-					if key < son.key :
-						son = son.left
-					else :
-						son = son.right
 
-				if son.key = None :
-					if key < father.key :
-						father.left = newNode
-					else :
-						father.right = newNode
+def insert_r(abb,key):
+	if abb == None:
+		newnode = createNode()
+		newnode.key = key
+		abb = newnode
+	elif abb.key == None:
+		abb.key = key
+	else:
+		if key < abb.key:
+			if abb.left == None:
+				abb.left = createNode()
+			insert_r(abb.left,key)
+		elif key > abb.key:
+			if abb.right == None:
+				abb.right = createNode()
+			insert_r(abb.right,key)
+			
+			
+
+
+#------------------------ Iterative Insert----------------------#
+def insert_i(abb,key):
+	new_node = createNode()
+	new_node.key = key
+	if abb.key == None:
+		abb = new_node			
+	else :
+		if new_node.key < abb.key :
+			son = abb
+			father = createNode()
+			while son.key != None and son.key != key:
+				father = son
+				if key < son.key :
+					son = son.left
+				else :
+					son = son.right
+					
+			if son.key == None :
+				if key < father.key :
+					father.left = new_node
+				else :
+					father.right = new_node
+						
+#------------------------Insert key----------------------#	
+def	insertKey(abb,key):
+	insert_r(abb,key)
+		
+#------------------------ Recursive Search----------------------#
+def search_r(abb,key):
+	if abb == None or abb.key == None:
+		return None
+	elif abb.key == key:
+		return abb
+	elif key < abb.key:
+		return search_r(abb.left,key)
+	else:
+		return search_r(abb.right,key)
+			
+#------------------------ Iterative Search----------------------#
+def search_i(abb,key):
+	node = abb
+	while node != None and node.key != key:
+		if key < node.key:
+			node = node.left
+		else:
+			node = node.right
+	return node
+
+#------------------------Search key----------------------#	
+def	searchKey(abb,key):
+	return search_r(abb,key)	
+
+#------------------------Left Son----------------------#	
+def leftSon(abb):
+	return abb.left
+		
+#------------------------Right Son----------------------#	
+def rightSon(abb):
+	return abb.right
+		
+#------------------------Key----------------------#	
+def key(abb):
+	return abb.key
+		
+#------------------------Left Son----------------------#	
+def isEmptyTree(abb):
+	return abb == None or abb.key == None
+		
+#------------------------ Recursive Erase----------------------#
+def erase_r(abb,key):
+	aux = createNode()
+
+	def sup2(tree):
+		if tree.right != None:
+			sup2(tree.right)
+		else:
+			aux.key = tree.key
+			aux = tree
+			tree = tree.left
+
+	if abb != None and abb.key != None:
+		if key < abb.key:
+			erase_r(abb.left,key)
+		elif key > abb.key:
+			erase_r(abb.right,key)
+		else:
+			aux = abb
+			if abb.left == None:
+				abb = abb.right
+			elif abb.right == None:
+				abb = abb.left
+			else:
+				sup2(abb.left)
+			aux = None
+			
+#------------------------ Iterative Erase----------------------#
+def erase_i(abb,key):
+	sons_num
+	sup
+	f_sup
+	not_empty_son
+	son_left_max
+		
+	f_sup = None
+	sup = abb
+	while sup != None and sup.key != key:
+		f_sup = sup
+		if key < sup.key:
+			sup = sup.left
+		else:
+			sup = sup.right
+				
+	if sup != None:
+		sons_num = 0
+		if sup.left != None:
+			sons_num = sons_num + 1
+		if sup.right != None:
+			sons_num = sons_num + 1
+				
+		if sons_num == 0:
+			if f_sup == None:
+				abb = None
+			elif f_sup.left == sup:
+				f_sup.left = None
+			else:
+				f_sup.right = None
+					
+		elif sons_num == 1:
+			if sup.left == None:
+				not_empty_son = sup.right
+			else:	
+				not_empty_son = sup.left
+				
+			if f_sup == None:
+				abb = not_empty_son
+			elif f_sup.left == sup:
+				f_sup.left = not_empty_son
+			else:
+				f_sup.right = not_empty_son
+				
+		else:
+			f_sup = sup
+			son_left_max = sup.left
+			while son_left_max.right != None:
+				f_sup = son_left_max
+				son_left_max = son_left_max.right
+				
+			sup.key = son_left_max.key
+			if f_sup == sup:
+				f_sup.left = son_left_max.left
+			else:
+				f_sup.right = son_left_max.left
+				
+			sup = son_left_max
+				
+		sup = None	
+		
+#------------------------ Erase Key----------------------#				
+def eraseKey(abb,key):
+	erase_r(abb,key)
+				
+		
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+		
+				
+				
+				
