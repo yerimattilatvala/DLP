7fdceb4b1c266a4b35499bad72602e0c4b693d29
drivera1 <danir964@gmail.com>
2017-09-30 22:04:29 +0200

    oCaml Tad iterativo y recursivo solucionado
    Te dejo el resto que me da palo :)

--- oCaml/TAD.ml
+++ oCaml/TAD.ml
@@ -1,6 +1,6 @@
 type intabb = 
     Empty 
-    | Node of int * intabb * intabb;;
+    | Node of int * intabb ref * intabb ref;;
 
 let error s = 
     print_endline s;
@@ -8,158 +8,157 @@ let error s =
 
 let emptyTree = Empty;;
 
-let isEmptyTree abb = match abb with
-Empty -> true
-|Node(_,_,_) -> false;;
-
-let key abb = match abb with
-Node(key,_,_) -> key ;;
-
-let leftSon abb = match abb with
-Empty -> Empty
-|Node(_,left,_) -> left;;
-
-let rightSon abb = match abb with
-Empty -> Empty
-|Node(_,_,right) -> right;;
-
-
-let rec insert_r abb data =  match abb with
-    Empty -> Node(data,Empty,Empty)
+let rec insert_r abb data =  match !abb with
+    Empty -> abb := Node(data,ref Empty,ref Empty)
     |Node(key,left,right) -> 
-        if data < key then Node(key,insert_r left data, right)
-        else if data>key then Node(key,left,insert_r right data)
-        else abb;;
+        if data < key then insert_r left data
+        else if data>key then insert_r right data;;
 
-let insertKey abb data = insert_r abb data;;
+let insert_i abb data = 
+    let newNode = Node(data,ref Empty,ref Empty) in
+    let father = ref (ref emptyTree) in
+    let son = ref (ref emptyTree) in
+    match !abb with
+        Empty -> abb := newNode
+        |Node(_,_,_)-> (
+            !father := emptyTree;
+            son := abb;
+            while (!(!son) != Empty) && ((key !(!son)) != data) do
+                father := !son;
+                (if data < (key !(!son)) then
+                    son := leftSon(!(!son))
+                else
+                    son := rightSon(!(!son)));
+            done;
+            (if !(!son) == Empty then
+                (if data < (key !(!father)) then
+                    leftSon !(!father) := newNode
+                else
+                    rightSon !(!father) := newNode));
+            ()
+            );;
 
-let rec search_r abb data = match abb with
+
+
+let rec search_r abb data = match !abb with
     Empty -> Empty
     |Node(k,left,right) ->
         if data < k then search_r left data
         else if data > k then search_r right data
-        else abb;;            
-let searchKey abb data = search_r abb data;;
+        else !abb;; 
 
 let search_i abb data =
-    let node = ref abb in
+    let node = ref !abb in
     while !node != Empty && (key !node) != data do
         if data < (key !node) then
-            node := (leftSon !node)
+            nodelet insertKey abb data = insert_r abb data;; := !(leftSon !node)
         else
-            node := (rightSon !node)
+            node := !(rightSon !node)
     done;
     !node;;
 
-    let rec erase_r abb data = 
-        
-                let rec sup2 tree = match tree with
-                    Node(k,l,r) -> 
-                        if r != Empty then let (a,b) = sup2 r in
-                            (a,Node(k,l,b))
-                        else
-                            (k,l)
-        
-                in
-            match abb with
-                Empty -> abb
-                |Node(k,l,r) ->
-                    if data < k then Node(k,erase_r l data,r)
-                    else if data > k then Node(k,l,erase_r r data)
-                    else match (l,r) with
-                        (Empty,_) -> r
-                        |(_,Empty) -> l 
-                        |(_,_) -> let (a,b) = sup2 l in
-                            Node(a,b,r);;
+let searchKey abb data = search_r abb data;;
 
-(*  Node(7,Node(3,Node(2,Empty,Empty),Node(5,Node(4,Empty,Empty),Node(6,Empty,Empty))),Node(8,Empty,Empty)) *)
+let isEmptyTree abb = match abb with
+    Empty -> true
+    |Node(_,_,_) -> false;;
 
-(*let erase_i abb data = 
+let key abb = match abb with
+    |Node(key,_,_) -> key ;;
+
+let leftSon abb = match abb with
+    Empty -> ref Empty
+    |Node(_,left,_) -> left;;
+
+let rightSon abb = match abb with
+    Empty -> ref Empty
+    |Node(_,_,right) -> right;;
+
+let rec erase_r abb data = 
+    let aux = ref abb in
+        let rec sup2 tree = match !tree with
+            Node(k,l,r) -> 
+                (if !r != Empty then sup2 r
+                else (
+                    !aux := Node(k,leftSon !(!aux),rightSon !(!aux));
+                    tree := !(leftSon !tree);
+                )
+                );()
+
+        in
+    match !abb with
+        Empty -> ()
+        |Node(k,l,r) ->
+            if data < k then erase_r l data
+            else if data > k then erase_r r data
+            else match (!l,!r) with
+                (Empty,_) -> abb := !r
+                |(_,Empty) -> abb := !l 
+                |(_,_) -> sup2 l;;
+                
+
+let erase_i abb data = 
     let sons_num = ref 0 in
     let sup = ref (ref Empty) in
-    let abb_out = ref (ref Empty) in
-    let abb_out2 = ref (ref Empty) in
     let f_sup = ref (ref Empty) in
     let not_empty_son = ref (ref Empty) in
     let son_left_max = ref (ref Empty) in
 
-    !abb_out := abb;
-    abb_out2 := !abb_out;
-    sup := !abb_out;
+    sup := abb;
     while !(!sup) != Empty && (key !(!sup)) != data do
         f_sup := !sup;
         if data < (key !(!sup)) then
-            sup := ref (leftSon !(!sup))
+            sup := leftSon !(!sup)
         else
-            sup := ref (rightSon !(!sup))
+            sup := rightSon !(!sup)
     done;
 
     (if !(!sup) != Empty then
         (sons_num := 0;
-        (if (leftSon !(!sup)) != Empty then
+        (if !(leftSon !(!sup)) != Empty then
             sons_num := !sons_num + 1);
-        (if (rightSon !(!sup)) != Empty then
+        (if !(rightSon !(!sup)) != Empty then
             sons_num := !sons_num + 1);
             
         (if !sons_num == 0 then
             (if !(!f_sup) == Empty then
-                !abb_out := Empty
-            else if (leftSon !(!f_sup)) == !(!sup) then
-                !f_sup := Node((key !(!f_sup)),Empty,(rightSon !(!f_sup)))
+                abb := Empty
+            else if !(leftSon !(!f_sup)) == !(!sup) then
+                (leftSon !(!f_sup)) := Empty
             else 
-                !f_sup := Node((key !(!f_sup)),(leftSon !(!f_sup)),Empty))
+                (rightSon !(!f_sup)) := Empty)
 
         else if !sons_num == 1 then
-            ((if (leftSon !(!sup)) == Empty then
-                !not_empty_son := (rightSon !(!sup))
+            ((if !(leftSon !(!sup)) == Empty then
+                not_empty_son := (rightSon !(!sup))
             else
-                !not_empty_son := (leftSon !(!sup)));
+                not_empty_son := (leftSon !(!sup)));
 
             (if !(!f_sup) == Empty then
-				!abb_out := !(!not_empty_son)
-            else if (leftSon !(!f_sup)) == !(!sup) then
-                !f_sup := Node((key !(!f_sup)),!(!not_empty_son),(rightSon !(!f_sup)))
+				abb := !(!not_empty_son)
+            else if !(leftSon !(!f_sup)) == !(!sup) then
+                (leftSon !(!f_sup)) := !(!not_empty_son)
             else
-                !f_sup := Node((key !(!f_sup)),(leftSon !(!f_sup)),!(!not_empty_son))))
+                (rightSon !(!f_sup)) := !(!not_empty_son)))
         
         else
-            (!f_sup := !(!sup);
-            !son_left_max := (leftSon !(!sup));
-            while (rightSon !(!son_left_max)) != Empty do
-                !f_sup := !(!son_left_max);
-                !son_left_max := (rightSon !(!son_left_max))
+            (f_sup := !sup;
+            son_left_max := (leftSon !(!sup));
+            while !(rightSon !(!son_left_max)) != Empty do
+                f_sup := !son_left_max;
+                son_left_max := (rightSon !(!son_left_max))
             done;
 
             !sup := Node((key !(!son_left_max)),(leftSon !(!sup)),(rightSon !(!sup)));
             (if  !(!f_sup) == !(!sup) then
-                !f_sup := Node((key !(!f_sup)),(leftSon !(!son_left_max)),(rightSon !(!f_sup)))
+                (leftSon !(!f_sup)) := !(leftSon !(!son_left_max))
             else
-                !f_sup := Node((key !(!f_sup)),(leftSon !(!f_sup)),(leftSon !(!son_left_max))));
+                (rightSon !(!f_sup)) := !(leftSon !(!son_left_max)));
             
-            !sup := !(!son_left_max))
+            sup := !son_left_max
+            )
         )
-        ));    
-        !(!abb_out2);;*)
+        ));
+        ();;
 
-(*let insert_i abb data = 
-    let newNode = Node(data,Empty,Empty) in
-    if abb = Empty then
-        newNode
-    else begin
-        let father = ref Empty in
-        let son = ref abb in
-        while !son != Empty && (key !son) != data do
-            father := !son;
-            if data < (key !son) then
-                son := (leftSon !son)
-            else
-                son:=(rightSon !son)
-        done;
-        if !son = Empty then
-            if data < key !father then 
-                (*padre^.izdo := nuevo*)
-            else 
-                (*padre^.dcho := nuevo;*)
-        else
-            abb
-end;;*)
+let eraseKey abb data = erase_i abb data;;
--- oCaml/abb_program.ml
+++ oCaml/abb_program.ml
@@ -1,63 +1,85 @@
 
-
 let rec preorderWithBracket abb = 
     print_string "(";
     if not (isEmptyTree abb ) then
-        if not (isEmptyTree (leftSon abb)) || not (isEmptyTree (rightSon abb)) then
+        if not (isEmptyTree !(leftSon abb)) || not (isEmptyTree !(rightSon abb)) then
             begin
                 print_string " ";
                 print_int (key abb);
                 print_string " ";
-                preorderWithBracket (leftSon abb);
+                preorderWithBracket !(leftSon abb);
                 print_string " ";
-                preorderWithBracket (rightSon abb);
+                preorderWithBracket !(rightSon abb);
+
             end
         else begin
             print_string " ";
             print_int (key abb);
             print_string " ";
-  
+
         end;
-    print_string ")";;
+        print_string ")";;
+
+let test =
+    let abb = ref emptyTree in
+    insertKey abb 4;
+    insertKey abb 4;
+    insertKey abb 2;
+    insertKey abb 6;
+    insertKey abb 1;
+    insertKey abb 3;
+    insertKey abb 5;
+    insertKey abb 7;
 
 
-let abb = emptyTree;;
-let abb = insertKey abb 4;;
-let abb = insertKey abb 4;;
-let abb = insertKey abb 2;;
-let abb = insertKey abb 6;;
-let abb = insertKey abb 1;;
-let abb = insertKey abb 3;;
-let abb = insertKey abb 5;;
-let abb = insertKey abb 7;;
+    preorderWithBracket !abb;
+    print_string "\n";
 
+    print_string " Buscar 1... ";
+    print_int (key(searchKey abb 1));
+    print_string "\n";
 
-preorderWithBracket abb;;
+    print_string " Buscar 2... ";
+    print_int (key(searchKey abb 2));
+    print_string "\n";
 
-print_string " Buscar 1... ";;
-print_int (key(searchKey abb 1));;
-print_string "\n";;
+    print_string " Buscar 3... ";
+    print_int (key(searchKey abb 3));
+    print_string "\n";
 
-print_string " Buscar 2... ";;
-print_int (key(searchKey abb 2));;
-print_string "\n";;
+    print_string " Buscar 4... ";
+    print_int (key(searchKey abb 4));
+    print_string "\n";
 
-print_string " Buscar3... ";;
-print_int (key(searchKey abb 3));;
-print_string "\n";;
+    print_string " Buscar 5... ";
+    print_int (key(searchKey abb 5));
+    print_string "\n";
 
-print_string " Buscar 4... ";;
-print_int (key(searchKey abb 4));;
-print_string "\n";;
+    print_string " Buscar 6... ";
+    print_int (key(searchKey abb 6));
+    print_string "\n";
 
-print_string " Buscar 5... ";;
-print_int (key(searchKey abb 5));;
-print_string "\n";;
+    print_string " Buscar 7... ";
+    print_int (key(searchKey abb 7));
+    print_string "\n";
 
-print_string " Buscar 6... ";;
-print_int (key(searchKey abb 6));;
-print_string "\n";;
+    print_string "remove 5...";
+    eraseKey abb 5;
+    preorderWithBracket !abb;
+    print_string "\n";
 
-print_string " Buscar 7... ";;
-print_int (key(searchKey abb 7));;
-print_string "\n";;
+    print_string "remove 6...";
+    eraseKey abb 6;
+    preorderWithBracket !abb;
+    print_string "\n";
+
+    print_string "remove 4...";
+    eraseKey abb 4;
+    preorderWithBracket !abb;
+    print_string "\n";
+
+    print_string "remove 2...";
+    eraseKey abb 2;
+    preorderWithBracket !abb;
+    print_string "\n";
+    ();;
